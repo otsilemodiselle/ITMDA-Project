2454d6843521b9b8195ae97c08cb9984779e23af
Otsile Modiselle <otsilemodiselle@outlook.com>
2025-03-23 11:49:08 +0200

    import FlashMessage library

--- sergeants_orders_app/App.tsx
+++ sergeants_orders_app/App.tsx
@@ -2,13 +2,25 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import AppText from './src/components/texts/AppText';
 import AppSafeView from './src/components/Views/AppSafeView';
+import FlashMessage, { showMessage } from 'react-native-flash-message';
 
 export default function App() {
   return (
-    <AppSafeView style={styles.container}>
-      <AppText variant='medium'>Hello World!</AppText>
-      <AppText variant='bold'>Hello World!</AppText>
-    </AppSafeView>
+    <>
+      <FlashMessage position={"top"}/>
+      <AppSafeView style={styles.container}>
+        <AppText variant='medium'>Hello World!</AppText>
+        <AppText 
+          variant='bold'
+          onPress={() => showMessage({
+            message:"Hello World!",
+            type: "warning"
+          })}
+        >
+          Hello World!
+        </AppText>
+      </AppSafeView>
+    </>
   );
 }
 
