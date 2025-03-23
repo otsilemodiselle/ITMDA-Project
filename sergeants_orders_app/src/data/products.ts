9ac350b6a88d7fcb51eaa63b76da1300f7ea6cb6
Otsile Modiselle <otsilemodiselle@outlook.com>
2025-03-23 21:04:49 +0200

    add product list file

---
+++ sergeants_orders_app/src/data/products.ts
@@ -0,0 +1,210 @@
+export const products =[
+{
+    id:1,
+    name: "6 Pieces & Chips",
+    price:160,
+    category: "Chicken",
+    description: "Flame grilled chicken pieces special sauce",
+    imageURL: "https://github.com/otsilemodiselle/sergeants-order-mobile/blob/20f59553c064c2b0dd5aeaa552eec260d9b53433/sergeants_orders_app/assets/1361.png?raw=true"
+},
+{
+    id:2,
+    name: "Sergeant's Chicken Burger",
+    price:60,
+    category: "Burgers",
+    description: "Stacked burger sandwich with crumbed patty and fixings",
+    imageURL: "https://github.com/otsilemodiselle/sergeants-order-mobile/blob/a92225f1f21e38fa691fb189fafd0717a73f742e/sergeants_orders_app/assets/1391.png?raw=true"
+},
+{
+    id:3,
+    name: "Sergeant's Chicken & Cheese Burger",
+    price:70,
+    category: "Burger",
+    description: "Stacked burger sandwich with crumbed patty, cheese and fixings",
+    imageURL: "https://github.com/otsilemodiselle/sergeants-order-mobile/blob/a92225f1f21e38fa691fb189fafd0717a73f742e/sergeants_orders_app/assets/1390.png?raw=true"
+},
+{
+    id:4,
+    name: "Sergeant's Mouthful Burger",
+    price:90,
+    category: "Burger",
+    description: "Stacked burger sandwich with crumbed patty, our famous mac n cheese and fixings",
+    imageURL: "https://github.com/otsilemodiselle/sergeants-order-mobile/blob/20f59553c064c2b0dd5aeaa552eec260d9b53433/sergeants_orders_app/assets/1360.png?raw=true"
+},
+{
+    id:5,
+    name: "Chips",
+    price:30,
+    category: "Sides",
+    description: "Golden brown, hand-cut, potato chips",
+    imageURL: "https://github.com/otsilemodiselle/sergeants-order-mobile/blob/a92225f1f21e38fa691fb189fafd0717a73f742e/sergeants_orders_app/assets/1394.png?raw=true"
+},
+{
+    id:6,
+    name: "Coleslaw",
+    price:25,
+    category: "Sides",
+    description: "Fresh and crunchy veg mix in tangy mayonnaise",
+    imageURL: "https://github.com/otsilemodiselle/sergeants-order-mobile/blob/20f59553c064c2b0dd5aeaa552eec260d9b53433/sergeants_orders_app/assets/1358.png?raw=true"
+},
+{
+    id:7,
+    name: "Sergeant's Famous Mac n Cheese",
+    price:30,
+    category: "Sides",
+    description: "Delicious macaroni in the Sergeant's famous cheesy sauce",
+    imageURL: "https://github.com/otsilemodiselle/sergeants-order-mobile/blob/20f59553c064c2b0dd5aeaa552eec260d9b53433/sergeants_orders_app/assets/1357.png?raw=true"
+},
+{
+    id:8,
+    name: "Chicken Nuggets",
+    price:45,
+    category: "Extras",
+    description: "Fluffy and crumbed chicken nuggets",
+    imageURL: "https://github.com/otsilemodiselle/sergeants-order-mobile/blob/a92225f1f21e38fa691fb189fafd0717a73f742e/sergeants_orders_app/assets/1393.png?raw=true"
+},
+{
+    id:9,
+    name: "Chicken Strips",
+    price:45,
+    category: "Extras",
+    description: "Crunchy and crumbed chicken strips",
+    imageURL: "https://github.com/otsilemodiselle/sergeants-order-mobile/blob/a92225f1f21e38fa691fb189fafd0717a73f742e/sergeants_orders_app/assets/1392.png?raw=true"
+},
+{
+    id:10,
+    name: "Soldiers Basket",
+    price:120,
+    category: "Baskets",
+    description: "Combo of chicken strips, cheese & am pillows, and chips",
+    imageURL: "https://github.com/otsilemodiselle/sergeants-order-mobile/blob/20f59553c064c2b0dd5aeaa552eec260d9b53433/sergeants_orders_app/assets/1368.png?raw=true"
+},
+{
+    id:11,
+    name: "Sergeant Basket",
+    price:150,
+    category: "Basket",
+    description: "Combo of baby Russian grillers, chicken strips, and pillows",
+    imageURL: "https://github.com/otsilemodiselle/sergeants-order-mobile/blob/20f59553c064c2b0dd5aeaa552eec260d9b53433/sergeants_orders_app/assets/1366.png?raw=true"
+},
+{
+    id:12,
+    name: "Napoli",
+    price:120,
+    category: "Pizza",
+    description: "Tomato base, mozzarella, basil",
+    imageURL: "https://github.com/otsilemodiselle/sergeants-order-mobile/blob/20f59553c064c2b0dd5aeaa552eec260d9b53433/sergeants_orders_app/assets/1376.png?raw=true"
+},
+{
+    id:13,
+    name: "Roma",
+    price:160,
+    category: "Pizza",
+    description: "Tomato base, chourizo, ham, pepperoni, garlic",
+    imageURL: "https://github.com/otsilemodiselle/sergeants-order-mobile/blob/20f59553c064c2b0dd5aeaa552eec260d9b53433/sergeants_orders_app/assets/1375.png?raw=true"
+},
+{
+    id:14,
+    name: "Capri",
+    price:135,
+    category: "Pizza",
+    description: "Tomato base, peppers, onions, olives",
+    imageURL: "https://github.com/otsilemodiselle/sergeants-order-mobile/blob/20f59553c064c2b0dd5aeaa552eec260d9b53433/sergeants_orders_app/assets/1374.png?raw=true"
+},
+{
+    id:15,
+    name: "Toscana",
+    price:145,
+    category: "Pizza",
+    description: "Tomato base, ham, mushrooms",
+    imageURL: "https://github.com/otsilemodiselle/sergeants-order-mobile/blob/20f59553c064c2b0dd5aeaa552eec260d9b53433/sergeants_orders_app/assets/1374.png?raw=true"
+},
+{
+    id:16,
+    name: "Sicilia",
+    price:145,
+    category: "Pizza",
+    description: "Tomato base, ham, pineapple",
+    imageURL: "https://github.com/otsilemodiselle/sergeants-order-mobile/blob/20f59553c064c2b0dd5aeaa552eec260d9b53433/sergeants_orders_app/assets/1370.png?raw=true"
+},
+{
+    id:17,
+    name: "Bologna",
+    price:165,
+    category: "Pizza",
+    description: "Tomato base, onion, chilli, green pepper, ham, mince",
+    imageURL: "https://github.com/otsilemodiselle/sergeants-order-mobile/blob/20f59553c064c2b0dd5aeaa552eec260d9b53433/sergeants_orders_app/assets/1369.png?raw=true"
+},
+{
+    id:18,
+    name: "Florence",
+    price:135,
+    category: "Pizza",
+    description: "Tomato base, onion, chicken breast pieces, and rosemary",
+    imageURL: "https://github.com/otsilemodiselle/sergeants-order-mobile/blob/20f59553c064c2b0dd5aeaa552eec260d9b53433/sergeants_orders_app/assets/1364.png?raw=true"
+},
+{
+    id:19,
+    name: "Cheese and Tomato Pillows",
+    price:30,
+    category: "Cusini (Pillows)",
+    description: "Cheese and tomato on puff pastry",
+    imageURL: "https://github.com/otsilemodiselle/sergeants-order-mobile/blob/20f59553c064c2b0dd5aeaa552eec260d9b53433/sergeants_orders_app/assets/1365.png?raw=true"
+},
+{
+    id:20,
+    name: "Cheese, Ham and Tomato Pillows",
+    price:30,
+    category: "Cusini (Pillows)",
+    description: "Cheese, ham and tomato on puff pastry",
+    imageURL: "https://github.com/otsilemodiselle/sergeants-order-mobile/blob/20f59553c064c2b0dd5aeaa552eec260d9b53433/sergeants_orders_app/assets/1367.png?raw=true"
+},
+{
+    id:21,
+    name: "Full Rotisserie Chicken, Chips, Coleslaw",
+    price:189,
+    category: "Chicken",
+    description: "Full filled chicken with special spices and optional chips",
+    imageURL: "https://github.com/otsilemodiselle/sergeants-order-mobile/blob/20f59553c064c2b0dd5aeaa552eec260d9b53433/sergeants_orders_app/assets/1362.png?raw=true"
+},
+{
+    id:22,
+    name: "Coke",
+    price:30,
+    category: "Drinks",
+    description: "440ml buddy",
+    imageURL: "https://github.com/otsilemodiselle/sergeants-order-mobile/blob/20f59553c064c2b0dd5aeaa552eec260d9b53433/sergeants_orders_app/assets/1354.png?raw=true"
+},
+{
+    id:23,
+    name: "Sprite",
+    price:30,
+    category: "Drinks",
+    description: "440ml buddy",
+    imageURL: "https://github.com/otsilemodiselle/sergeants-order-mobile/blob/20f59553c064c2b0dd5aeaa552eec260d9b53433/sergeants_orders_app/assets/1380.png?raw=true"
+},
+{
+    id:24,
+    name: "Rhodes Fruit Juice",
+    price:15,
+    category: "Drinks",
+    description: "200ml juice",
+    imageURL: "https://github.com/otsilemodiselle/sergeants-order-mobile/blob/20f59553c064c2b0dd5aeaa552eec260d9b53433/sergeants_orders_app/assets/1379.png?raw=true"
+},
+{
+    id:25,
+    name: "Still Water",
+    price:20,
+    category: "Drinks",
+    description: "500ml still water",
+    imageURL: "https://github.com/otsilemodiselle/sergeants-order-mobile/blob/20f59553c064c2b0dd5aeaa552eec260d9b53433/sergeants_orders_app/assets/1381.png?raw=true"
+},
+{
+    id:26,
+    name: "Sparking Water",
+    price:20,
+    category: "Drinks",
+    description: "500ml sparkling water",
+    imageURL: "https://github.com/otsilemodiselle/sergeants-order-mobile/blob/20f59553c064c2b0dd5aeaa552eec260d9b53433/sergeants_orders_app/assets/1381.png?raw=true"
+}
+]
