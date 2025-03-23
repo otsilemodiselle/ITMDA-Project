5f09d85d0793a5c47eae2cba63254b09f556fa2b
Otsile Modiselle <otsilemodiselle@outlook.com>
2025-03-23 11:40:27 +0200

    create an SafeView component

--- sergeants_orders_app/App.tsx
+++ sergeants_orders_app/App.tsx
@@ -1,21 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import AppText from './src/components/texts/AppText';
+import AppSafeView from './src/components/Views/AppSafeView';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <AppSafeView style={styles.container}>
       <AppText variant='medium'>Hello World!</AppText>
       <AppText variant='bold'>Hello World!</AppText>
-    </View>
+    </AppSafeView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#121212',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // flex: 1,
+    // backgroundColor: '#121212',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
 });
---
+++ sergeants_orders_app/src/components/Views/AppSafeView.tsx
@@ -0,0 +1,37 @@
+import {
+  Platform,
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  View,
+  ViewStyle,
+} from "react-native";
+import React, { FC, ReactNode } from "react";
+import { AppColors } from "../../styles/colors";
+import { IS_Android } from "../../constants/constants";
+
+interface AppSafeViewProps {
+  children: ReactNode;
+  style: ViewStyle;
+}
+
+const AppSaveView: FC<AppSafeViewProps> = ({ children, style }) => {
+  return (
+    <SafeAreaView style={styles.safeArea}>
+      <View style={[styles.container, style]}>{children}</View>
+    </SafeAreaView>
+  );
+};
+
+export default AppSaveView;
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    backgroundColor: AppColors.mainBackground,
+    paddingTop: IS_Android ? StatusBar.currentHeight || 0 : 0,
+  },
+  container: {
+    flex: 1,
+  },
+});
