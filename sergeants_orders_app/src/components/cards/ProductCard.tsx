133de3db3fc190435ad5457e5fb37793b155cfec
Otsile Modiselle <otsilemodiselle@outlook.com>
2025-03-24 21:43:51 +0200

    further update product card design

--- sergeants_orders_app/src/components/cards/ProductCard.tsx
+++ sergeants_orders_app/src/components/cards/ProductCard.tsx
@@ -1,93 +1,92 @@
-import { StyleSheet, Text, View, Image } from 'react-native'
-import React from 'react'
-import { s, vs } from 'react-native-size-matters'
-import { AppColors } from '../../styles/colors'
-import AppText from '../texts/AppText'
-import { Ionicons } from "@expo/vector-icons"
-import { AppFonts } from '../../styles/fonts'
+import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
+import React from "react";
+import { s, vs } from "react-native-size-matters";
+import { AppColors } from "../../styles/colors";
+import AppText from "../texts/AppText";
+import { Ionicons } from "@expo/vector-icons";
+import { AppFonts } from "../../styles/fonts";
+import { commonStyles } from "../../styles/sharedStyles";
 
 const ProductCard = () => {
   return (
-    <View style={styles.cardsContainer}>
-        <View style={styles.productCard}>
-            <View style={styles.buyIconContainer}>
-            <Ionicons name="cart" size={30} color={AppColors.accentGray} />
-            </View>
-            <View style={styles.prodInfoContainer}>
-                <AppText style={styles.productName}>Full Rotisserie</AppText>
-                <AppText style={styles.price}>R189.00</AppText>
-            </View>
-            <View style={styles.imageContainer}>
-                <Image style={styles.productImage} source={require("../../assets/images/rotisserie.png")}/>
-            </View>
+    
+      <View style={styles.productCard}>
+        <TouchableOpacity style={styles.addToCartButton}>
+          <Ionicons name="cart" size={30} color={AppColors.accentGray} />
+        </TouchableOpacity>
+        <View style={styles.prodInfoContainer}>
+          <AppText style={styles.productName}>Full Rotisserie</AppText>
+          <AppText style={styles.price}>R189.00</AppText>
         </View>
-        
-        
-        
-    </View>
-  )
-}
+        <View style={styles.imageContainer}>
+          <Image
+            style={styles.productImage}
+            source={require("../../assets/images/rotisserie.png")}
+          />
+        </View>
+      </View>
+   
+  );
+};
 
-export default ProductCard
+export default ProductCard;
 
 const styles = StyleSheet.create({
-    cardsContainer:{
-        flex:1,
-        flexDirection: "column",
-        // flexWrap: "wrap",
-        justifyContent: "flex-start",
-        alignItems: "center"
-    },
-    productCard:{
-        flexDirection: "row",
-        width: "90%",
-        height: vs(130),
-        backgroundColor: AppColors.surface,
-        borderRadius:s(5),
-        margin: s(10),
-        alignItems: "center",
-        padding: s(10)
-    },
-    prodInfoContainer:{
-        height: vs(130),
-        flex:1,
-        // borderWidth: 1,
-        // borderColor: "white",
-        justifyContent: "space-between"
-    },
-    productName:{
-        marginTop:vs(10),
-        marginStart:vs(20)
-    },
-    price:{
-        marginBottom:vs(10),
-        marginStart:vs(20),
-        fontFamily: AppFonts.Bold
-    },
-    buyIconContainer:{
-        height: vs(130),
-        width: s(30),
-        // borderWidth: 1,
-        // borderColor: "white",
-        alignItems: "center",
-        justifyContent: 'center',
-    },
-    imageContainer:{
-        overflow:"hidden",
-        borderTopLeftRadius: s(5),
-        borderTopRightRadius: s(5),
-        height: vs(100),
-        width: s(100),
-        // borderWidth: 1,
-        // borderColor: "white",
-    },
-    productImage:{
-        height:"100%",
-        width:"100%",
-        padding: 10,
-        transform:[
-            {scaleY:1.1},
-            {translateY:0}
-        ]
-    }
-})
+  cardsContainer: {
+    flex: 1,
+    flexDirection: "column",
+    // flexWrap: "wrap",
+    justifyContent: "flex-start",
+    alignItems: "center",
+  },
+  productCard: {
+    flexDirection: "row",
+    width: "90%",
+    height: vs(130),
+    backgroundColor: AppColors.surface,
+    borderRadius: s(5),
+    margin: s(10),
+    alignItems: "center",
+    padding: s(10),
+    ...commonStyles.shadow,
+  },
+  prodInfoContainer: {
+    height: vs(130),
+    flex: 1,
+    // borderWidth: 1,
+    // borderColor: "white",
+    justifyContent: "space-between",
+  },
+  productName: {
+    marginTop: vs(10),
+    marginStart: vs(20),
+  },
+  price: {
+    marginBottom: vs(10),
+    marginStart: vs(20),
+    fontFamily: AppFonts.Bold,
+  },
+  addToCartButton: {
+    height: vs(130),
+    width: s(30),
+    // borderWidth: 1,
+    // borderColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  imageContainer: {
+    overflow: "hidden",
+    borderTopLeftRadius: s(5),
+    borderTopRightRadius: s(5),
+    height: vs(100),
+    width: s(100),
+    // borderWidth: 1,
+    // borderColor: "white",
+  },
+  productImage: {
+    height: "100%",
+    width: "100%",
+    padding: 10,
+    transform: [{ scaleY: 1.1 }, { translateY: 0 }],
+  },
+});
--- sergeants_orders_app/src/styles/sharedStyles.ts
+++ sergeants_orders_app/src/styles/sharedStyles.ts
@@ -1,5 +1,19 @@
-import {vs, s} from "react-native-size-matters";
+import { vs, s } from "react-native-size-matters";
+import { AppColors } from "./colors";
+import {StyleSheet} from "react-native"
 
-export const sharedPaddingHorizontal = s(12)
-export const sharedMarginBottomSmall = s(12)
-export const sharedMarginBottomMedium = s(16)
+export const sharedPaddingHorizontal = s(12);
+export const sharedMarginBottomSmall = s(12);
+export const sharedMarginBottomMedium = s(16);
+export const commonStyles = StyleSheet.create({
+  shadow: {
+    // IOS
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 4 },
+    shadowOpacity: 0.2,
+    shadowRadius: 4,
+
+    //Android
+    elevation: 4,
+  },
+});
