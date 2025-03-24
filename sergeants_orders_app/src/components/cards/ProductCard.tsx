3996faa22bb2a84e7f905b5cb9a642e0920c34f5
Otsile Modiselle <otsilemodiselle@outlook.com>
2025-03-24 21:19:40 +0200

    complete product card design

--- sergeants_orders_app/src/components/cards/ProductCard.tsx
+++ sergeants_orders_app/src/components/cards/ProductCard.tsx
@@ -2,20 +2,129 @@ import { StyleSheet, Text, View, Image } from 'react-native'
 import React from 'react'
 import { s, vs } from 'react-native-size-matters'
 import { AppColors } from '../../styles/colors'
+import AppText from '../texts/AppText'
+import { Ionicons } from "@expo/vector-icons"
 
 const ProductCard = () => {
   return (
     <View style={styles.cardsContainer}>
-        <View style={styles.productContainer}>
+        <View style={styles.productCard}>
+            <View style={styles.buyIconContainer}>
+            <Ionicons name="cart" size={20} color={AppColors.accentGray} />
+            </View>
+            <View style={styles.prodInfoContainer}>
+                <AppText style={styles.productName}>Full Rotisserie</AppText>
+                <AppText style={styles.price}>R189.00</AppText>
+            </View>
             <View style={styles.imageContainer}>
-                <Image style={styles.productImage} source={{uri:"https://github.com/otsilemodiselle/sergeants-order-mobile/blob/20f59553c064c2b0dd5aeaa552eec260d9b53433/sergeants_orders_app/assets/1370.png?raw=true"}}/>
+                <Image style={styles.productImage} source={require("../../assets/images/rotisserie.png")}/>
             </View>
         </View>
-        <View style={styles.productContainer}>
+        
+        <View style={styles.productCard}>
+            <View style={styles.buyIconContainer}>
+            <Ionicons name="cart" size={20} color={AppColors.accentGray} />
+            </View>
+            <View style={styles.prodInfoContainer}>
+                <AppText style={styles.productName}>Sergeant's Basket</AppText>
+                <AppText style={styles.price}>R150.00</AppText>
+            </View>
             <View style={styles.imageContainer}>
-            <Image style={styles.productImage} source={{uri:"https://github.com/otsilemodiselle/sergeants-order-mobile/blob/20f59553c064c2b0dd5aeaa552eec260d9b53433/sergeants_orders_app/assets/1365.png?raw=true"}}/>
+                <Image style={styles.productImage} source={require("../../assets/images/sergeants_basket.png")}/>
             </View>
         </View>
+
+        <View style={styles.productCard}>
+            <View style={styles.buyIconContainer}>
+            <Ionicons name="cart" size={20} color={AppColors.accentGray} />
+            </View>
+            <View style={styles.prodInfoContainer}>
+                <AppText style={styles.productName}>Pizza Roma</AppText>
+                <AppText style={styles.price}>R160.00</AppText>
+            </View>
+            <View style={styles.imageContainer}>
+                <Image style={styles.productImage} source={require("../../assets/images/roma.png")}/>
+            </View>
+        </View>    
+
+        <View style={styles.productCard}>
+            <View style={styles.buyIconContainer}>
+            <Ionicons name="cart" size={20} color={AppColors.accentGray} />
+            </View>
+            <View style={styles.prodInfoContainer}>
+                <AppText style={styles.productName}>Pizza Capri</AppText>
+                <AppText style={styles.price}>R135.00</AppText>
+            </View>
+            <View style={styles.imageContainer}>
+                <Image style={styles.productImage} source={require("../../assets/images/capri.png")}/>
+            </View>
+        </View>
+        
+        <View style={styles.productCard}>
+            <View style={styles.buyIconContainer}>
+            <Ionicons name="cart" size={20} color={AppColors.accentGray} />
+            </View>
+            <View style={styles.prodInfoContainer}>
+                <AppText style={styles.productName}>Pizza Bologna</AppText>
+                <AppText style={styles.price}>R165.00</AppText>
+            </View>
+            <View style={styles.imageContainer}>
+                <Image style={styles.productImage} source={require("../../assets/images/bologna.png")}/>
+            </View>
+        </View>    
+
+        <View style={styles.productCard}>
+            <View style={styles.buyIconContainer}>
+            <Ionicons name="cart" size={20} color={AppColors.accentGray} />
+            </View>
+            <View style={styles.prodInfoContainer}>
+                <AppText style={styles.productName}>Sergeant's Chicken Burger</AppText>
+                <AppText style={styles.price}>R60.00</AppText>
+            </View>
+            <View style={styles.imageContainer}>
+                <Image style={styles.productImage} source={require("../../assets/images/chicken_burger.png")}/>
+            </View>
+        </View>
+        
+        <View style={styles.productCard}>
+            <View style={styles.buyIconContainer}>
+            <Ionicons name="cart" size={20} color={AppColors.accentGray} />
+            </View>
+            <View style={styles.prodInfoContainer}>
+                <AppText style={styles.productName}>Sergeant's Chicken and Cheese Burger</AppText>
+                <AppText style={styles.price}>R70.00</AppText>
+            </View>
+            <View style={styles.imageContainer}>
+                <Image style={styles.productImage} source={require("../../assets/images/chicken_burger_with_cheese.png")}/>
+            </View>
+        </View>
+        
+        <View style={styles.productCard}>
+            <View style={styles.buyIconContainer}>
+            <Ionicons name="cart" size={20} color={AppColors.accentGray} />
+            </View>
+            <View style={styles.prodInfoContainer}>
+                <AppText style={styles.productName}>Sergeant's Chicken Burger</AppText>
+                <AppText style={styles.price}>R60.00</AppText>
+            </View>
+            <View style={styles.imageContainer}>
+                <Image style={styles.productImage} source={require("../../assets/images/chicken_burger.png")}/>
+            </View>
+        </View>
+        
+        <View style={styles.productCard}>
+            <View style={styles.buyIconContainer}>
+            <Ionicons name="cart" size={20} color={AppColors.accentGray} />
+            </View>
+            <View style={styles.prodInfoContainer}>
+                <AppText style={styles.productName}>Sergeant's Chicken Burger</AppText>
+                <AppText style={styles.price}>R60.00</AppText>
+            </View>
+            <View style={styles.imageContainer}>
+                <Image style={styles.productImage} source={require("../../assets/images/chicken_burger.png")}/>
+            </View>
+        </View>
+        
         
     </View>
   )
@@ -26,33 +135,57 @@ export default ProductCard
 const styles = StyleSheet.create({
     cardsContainer:{
         flex:1,
-        flexDirection: "row",
-        flexWrap: "wrap",
-        justifyContent: "space-around",
-        alignItems: "flex-start"
+        flexDirection: "column",
+        // flexWrap: "wrap",
+        justifyContent: "flex-start",
+        alignItems: "center"
     },
-    productContainer:{
-        width: s(150),
-        height: vs(190),
+    productCard:{
+        flexDirection: "row",
+        width: "90%",
+        height: vs(130),
         backgroundColor: AppColors.surface,
         borderRadius:s(5),
         margin: s(10),
-        shadowColor: '#000',
-        shadowOffset: { width: 0, height: 4 },
-        shadowOpacity: 0.1,
-        shadowRadius: 6,
-        elevation: 5,
+        alignItems: "center",
+        padding: s(10)
+    },
+    prodInfoContainer:{
+        height: vs(130),
+        flex:1,
+        // borderWidth: 1,
+        // borderColor: "white",
+        justifyContent: "space-between"
+    },
+    productName:{
+        marginTop:vs(10),
+        marginStart:vs(20)
+    },
+    price:{
+        marginBottom:vs(10),
+        marginStart:vs(20)
+    },
+    buyIconContainer:{
+        height: vs(130),
+        width: s(30),
+        // borderWidth: 1,
+        // borderColor: "white",
+        alignItems: "center",
+        justifyContent: 'center',
     },
     imageContainer:{
         overflow:"hidden",
         borderTopLeftRadius: s(5),
         borderTopRightRadius: s(5),
-        height: vs(130),
-        width: "100%"
+        height: vs(100),
+        width: s(100),
+        // borderWidth: 1,
+        // borderColor: "white",
     },
     productImage:{
         height:"100%",
         width:"100%",
+        padding: 10,
         transform:[
             {scaleY:1.1},
             {translateY:0}
--- sergeants_orders_app/src/constants/images-paths.ts
+++ sergeants_orders_app/src/constants/images-paths.ts
@@ -1,7 +1,8 @@
 
 
 export const IMAGES = {
-    appLogo: require("../assets/images/logo3.png")
+    appLogo: require("../assets/images/logo3.png"),
+    appChickenBurger: require("../assets/images/chicken_burger.png")
 
     
 }
