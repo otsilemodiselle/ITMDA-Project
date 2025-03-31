9277b2e8e4afe43eca98651d3b5562b8b23b9289
Otsile Modiselle <otsilemodiselle@outlook.com>
2025-03-31 19:31:58 +0200

    create dummy order data

---
+++ sergeants_orders_app/src/data/orders.ts
@@ -0,0 +1,50 @@
+export const orders = [
+    {
+        id: 1003,
+        customerID: 649,
+        date: "2025-01-13",
+        cost: 450,
+    },
+    {
+        id: 1057,
+        customerID: 709,
+        date: "2025-01-19",
+        cost: 150,
+    },
+    {
+        id: 2001,
+        customerID: 649,
+        date: "2025-01-24",
+        cost: 300,
+    },
+    {
+        id: 2604,
+        customerID: 547,
+        date: "2025-01-31",
+        cost: 165,
+    },
+    {
+        id: 2841,
+        customerID: 709,
+        date: "2025-02-04",
+        cost: 240,
+    },
+    {
+        id: 3211,
+        customerID: 709,
+        date: "2025-02-14",
+        cost: 140,
+    },
+    {
+        id: 4087,
+        customerID: 649,
+        date: "2025-02-20",
+        cost: 360,
+    },
+    {
+        id: 4129,
+        customerID: 547,
+        date: "2025-02-28",
+        cost: 450,
+    },
+]
