b14bb007352193e57c419bf5a49b8c6e66993aa2
Otsile Modiselle <otsilemodiselle@outlook.com>
2025-04-01 19:14:06 +0200

    create addItemToCart cart slice action

--- sergeants_orders_app/src/store/reducers/cartSlice.ts
+++ sergeants_orders_app/src/store/reducers/cartSlice.ts
@@ -1,6 +1,18 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = {
+interface CartItem {
+    id: number | string;
+    name: string;
+    qty: number;
+    sum: number;
+    price: number;
+}
+
+interface CartState {
+    items: CartItem[]
+}
+
+const initialState : CartState = {
   items: [],
 };
 
@@ -9,12 +21,32 @@ const cartSlice = createSlice({
   initialState: initialState,
   reducers: {
     // addItemToCart
+    addItemToCart: (state, action) => {
+
+        const existingItem = state.items.find(
+            item => item.id === action.payload.id
+        )
+
+        if (existingItem) {
+            existingItem.qty += 1;
+            existingItem.sum += action.payload.price
+        }
+        else {
+            state.items.push({
+                ...action.payload,
+                qty: 1,
+                sum: action.payload.price
+            })
+        }
+
+        
+    }
     // removeItemFromCart
     // removeProductFromCart
     // emptyCart
   },
 });
 
-export const {} = cartSlice.actions;
+export const { addItemToCart } = cartSlice.actions;
 
 export default cartSlice.reducer;
