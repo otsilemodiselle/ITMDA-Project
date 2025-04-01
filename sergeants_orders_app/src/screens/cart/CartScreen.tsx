43dee7ce7a1acfbfd4057a73899c1603afeb49e6
Otsile Modiselle <otsilemodiselle@outlook.com>
2025-04-01 20:53:03 +0200

    implement removeItemFromCart cart slice action on cart screen

--- sergeants_orders_app/src/screens/cart/CartScreen.tsx
+++ sergeants_orders_app/src/screens/cart/CartScreen.tsx
@@ -1,42 +1,50 @@
-import { FlatList, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import HomeHeader from '../../components/headers/homeHeader'
-import AppSafeView from '../../components/Views/AppSafeView'
-import EmptyCart from './EmptyCart'
-import CartItem from '../../components/cart/CartItem'
-import TotalsView from '../../components/cart/TotalsView'
-import {productsFlat} from '../../data/productsFlat'
-import { sharedPaddingHorizontal } from '../../styles/sharedStyles'
-import { s } from 'react-native-size-matters'
-import AppButton from '../../components/buttons/AppButton'
-import { AppColors } from '../../styles/colors'
-import { useNavigation } from '@react-navigation/native'
-import { RootState } from '../../store/store'
-import { useSelector } from 'react-redux'
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import React from "react";
+import HomeHeader from "../../components/headers/homeHeader";
+import AppSafeView from "../../components/Views/AppSafeView";
+import EmptyCart from "./EmptyCart";
+import CartItem from "../../components/cart/CartItem";
+import TotalsView from "../../components/cart/TotalsView";
+import { productsFlat } from "../../data/productsFlat";
+import { sharedPaddingHorizontal } from "../../styles/sharedStyles";
+import { s } from "react-native-size-matters";
+import AppButton from "../../components/buttons/AppButton";
+import { AppColors } from "../../styles/colors";
+import { useNavigation } from "@react-navigation/native";
+import { RootState } from "../../store/store";
+import { useDispatch, useSelector } from "react-redux";
+import { removeItemFromCart } from "../../store/reducers/cartSlice";
 
 const CartScreen = () => {
-  const navigation = useNavigation()
-  const {items} = useSelector((state: RootState) => state.cartSlice)
+  const navigation = useNavigation();
+  const { items } = useSelector((state: RootState) => state.cartSlice);
+  const dispatch = useDispatch();
   return (
-    <AppSafeView >
-        <HomeHeader/>
-        <FlatList style={{paddingHorizontal: s(2)}}
-          data={items}
-          keyExtractor={(item) => item.id.toString()}
-          renderItem={({item}) => {
-            return <CartItem {...item} price={item.sum}/>
-          }}
-        />
-        <TotalsView/>
-        
+    <AppSafeView>
+      <HomeHeader />
+      <FlatList
+        style={{ paddingHorizontal: s(2) }}
+        data={items}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => {
+          return (
+            <CartItem
+              {...item}
+              price={item.sum}
+              onReducedPress={() => dispatch(removeItemFromCart(item))}
+            />
+          );
+        }}
+      />
+      <TotalsView />
     </AppSafeView>
-  )
-}
+  );
+};
 
-export default CartScreen
+export default CartScreen;
 
 const styles = StyleSheet.create({
-  checkoutButton:{
-    backgroundColor:AppColors.surfaceHover
-  }
-})
+  checkoutButton: {
+    backgroundColor: AppColors.surfaceHover,
+  },
+});
