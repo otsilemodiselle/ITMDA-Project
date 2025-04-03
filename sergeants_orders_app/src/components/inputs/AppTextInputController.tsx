5b0f9583bc5f4ba74d35c596cee6dabc0a6e2818
Otsile Modiselle <otsilemodiselle@outlook.com>
2025-04-03 19:51:20 +0200

    configure error message conditional styling to input text controller

--- sergeants_orders_app/src/components/inputs/AppTextInputController.tsx
+++ sergeants_orders_app/src/components/inputs/AppTextInputController.tsx
@@ -2,6 +2,9 @@ import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { Controller } from "react-hook-form";
 import AppTextInput from "./AppTextInput";
+import { AppColors } from "../../styles/colors";
+import AppText from "../texts/AppText";
+import { s, vs } from "react-native-size-matters";
 
 const AppTextInputController = ({
   control,
@@ -17,13 +20,19 @@ const AppTextInputController = ({
       name={name}
       rules={rules}
       render={({ field: { onChange, value }, fieldState: { error } }) => (
-        <AppTextInput
-          value={value}
-          onChangeText={onChange}
-          placeholder={placeholder}
-          secureTextEntry={secureTextEntry}
-          keyboardType={keyboardType}
-        />
+        <>
+          <AppTextInput
+            value={value}
+            onChangeText={onChange}
+            placeholder={placeholder}
+            secureTextEntry={secureTextEntry}
+            keyboardType={keyboardType}
+            style={error && styles.inputError}
+          />
+          {error && (
+            <AppText style={styles.errorMessage}>{error.message}</AppText>
+          )}
+        </>
       )}
     />
   );
@@ -31,4 +40,15 @@ const AppTextInputController = ({
 
 export default AppTextInputController;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  inputError: {
+    borderColor: AppColors.accentRed,
+  },
+  errorMessage: {
+    color: AppColors.accentRed,
+    fontSize: s(12),
+    marginBottom: vs(10),
+    marginTop: -vs(10),
+    marginLeft: s(16),
+  },
+});
