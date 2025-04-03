184c8067565fca98025a1c2573eda988296d55de
Otsile Modiselle <otsilemodiselle@outlook.com>
2025-04-03 21:44:37 +0200

    implement text input controller on login screen

--- sergeants_orders_app/src/screens/auth/SignInScreen.tsx
+++ sergeants_orders_app/src/screens/auth/SignInScreen.tsx
@@ -13,23 +13,67 @@ import { useState } from "react";
 import AppButton from "../../components/buttons/AppButton";
 import { AppColors } from "../../styles/colors";
 import { useNavigation } from "@react-navigation/native";
+import AppTextInputController from "../../components/inputs/AppTextInputController";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useForm } from "react-hook-form";
+
+const schema = yup.object({
+  inputtedLoginEmail: yup
+    .string()
+    .required("Email is required")
+    .email("Enter a valid email address"),
+
+  inputtedLoginPassword: yup
+    .string()
+    .required("Password is required")
+    .min(8, "Password must be at least 8 characters")
+    .matches(/[A-Z]/, "Password must contain at least one uppercase letter")
+    .matches(/[a-z]/, "Password must contain at least one lowercase letter")
+    .matches(/\d/, "Password must contain at least one number")
+    .matches(
+      /[@$!%*?&]/,
+      "Password must contain at least one special character"
+    ),
+});
+
+type FormData = yup.InferType<typeof schema>;
 
 const SignInScreen = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const navigation = useNavigation()
+  const navigation = useNavigation();
+  const { control, handleSubmit } = useForm({
+    resolver: yupResolver(schema),
+  });
+  const attemptLogin = (formData: FormData) => {
+    console.log(formData);
+  };
 
   return (
     <AppSafeView style={styles.container}>
       <Image source={IMAGES.appLogo} style={styles.logo} />
-      <AppTextInput placeholder="Email" onChangeText={setEmail} />
-      <AppTextInput
+      <AppTextInputController
+        placeholder="Email"
+        onChangeText={setEmail}
+        control={control}
+        name={"inputtedLoginEmail"}
+      />
+      <AppTextInputController
         placeholder="Password"
         onChangeText={setPassword}
         secureTextEntry
+        control={control}
+        name={"inputtedLoginPassword"}
       />
       <AppText style={styles.appName}>Sergeant's Orders!</AppText>
-      <AppButton title="Login"  onPress={() => navigation.navigate("MainAppBottomTabs")}/>
+      <AppButton
+        title="Login"
+        onPress={handleSubmit((data) => {
+          attemptLogin(data);
+          navigation.navigate("MainAppBottomTabs");
+        })}
+      />
       <AppButton
         title="Sign Up"
         style={styles.registerButton}
