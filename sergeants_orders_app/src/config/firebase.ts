50905739d764fdedce21bf0415bfd0fb3df7318a
Otsile Modiselle <otsilemodiselle@outlook.com>
2025-04-05 18:19:51 +0200

    amend firebase config file by activating getFirestore for product fetching

--- sergeants_orders_app/src/config/firebase.ts
+++ sergeants_orders_app/src/config/firebase.ts
@@ -1,10 +1,11 @@
 import { initializeApp } from 'firebase/app';
 import {getAuth} from 'firebase/auth';
+import {getFirestore} from 'firebase/firestore';
 
 // Optionally import the services that you want to use
 
 // import {...} from 'firebase/database';
-// import {...} from 'firebase/firestore';
+
 // import {...} from 'firebase/functions';
 // import {...} from 'firebase/storage';
 
@@ -22,7 +23,8 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 const auth = getAuth(app)
+const db = getFirestore(app)
 
-export {auth}
+export {auth,db}
 // For more information on how to access Firebase in your project,
 // see the Firebase documentation: https://firebase.google.com/docs/web/setup#access-firebase
