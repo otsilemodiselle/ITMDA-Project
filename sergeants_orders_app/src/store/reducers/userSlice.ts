0aeb3b0ca3bf368d00da550cffa3e7a60ed6ee5f
Otsile Modiselle <otsilemodiselle@outlook.com>
2025-04-05 19:14:12 +0200

    create a user slice reducer

---
+++ sergeants_orders_app/src/store/reducers/userSlice.ts
@@ -0,0 +1,24 @@
+
+
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface UserState {
+    userData: object;
+}
+
+const initialState: UserState = {
+    userData: {}
+}
+
+const userSlice = createSlice ({
+    name: "userData",
+    initialState,
+    reducers:{
+        setUserData: (state, action: PayloadAction<object>) =>{
+            state.userData=action.payload
+        }
+    }
+})
+
+export const {setUserData} = userSlice.actions
+export default userSlice.reducer
