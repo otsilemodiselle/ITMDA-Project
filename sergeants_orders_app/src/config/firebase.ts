910c6545c36aa14a89a635e57bfef81a4748687e
Otsile Modiselle <otsilemodiselle@outlook.com>
2025-04-05 09:03:05 +0200

    create a firebase auth config file

--- sergeants_orders_app/src/config/firebase.ts
+++ sergeants_orders_app/src/config/firebase.ts
@@ -1,7 +1,8 @@
 import { initializeApp } from 'firebase/app';
+import {getAuth} from 'firebase/auth';
 
 // Optionally import the services that you want to use
-// import {...} from 'firebase/auth';
+
 // import {...} from 'firebase/database';
 // import {...} from 'firebase/firestore';
 // import {...} from 'firebase/functions';
@@ -19,5 +20,9 @@ const firebaseConfig = {
   };
 
 const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app)
+
+export {auth}
 // For more information on how to access Firebase in your project,
 // see the Firebase documentation: https://firebase.google.com/docs/web/setup#access-firebase
