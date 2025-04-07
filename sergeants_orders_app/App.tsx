e2b3b6419b72098b98de590e9b32e368dec5c232
Otsile Modiselle <otsilemodiselle@outlook.com>
2025-04-07 20:52:34 +0200

    wrap main app in i18next languages provider elements

--- sergeants_orders_app/App.tsx
+++ sergeants_orders_app/App.tsx
@@ -1,36 +1,37 @@
 import { ActivityIndicator, StyleSheet, Text, View } from "react-native";
 import FlashMessage, { showMessage } from "react-native-flash-message";
-import {NavigationContainer} from "@react-navigation/native";
+import { NavigationContainer } from "@react-navigation/native";
 import MainAppStack from "./src/navigation/MainAppStack";
 import { useFonts } from "expo-font";
 import { store } from "./src/store/store";
 import { Provider } from "react-redux";
+import i18n from "./src/localization/i18n";
+import { I18nextProvider } from "react-i18next";
 
 export default function App() {
-
   const [fontsLoaded] = useFonts({
     "Roboto-Bold": require("./src/assets/fonts/Roboto-Bold.ttf"),
-    "Roboto-Medium": require("./src/assets/fonts/Roboto-Medium.ttf")
-  })
+    "Roboto-Medium": require("./src/assets/fonts/Roboto-Medium.ttf"),
+  });
 
-  if(!fontsLoaded) {
-    return <ActivityIndicator size={"large"}/>
+  if (!fontsLoaded) {
+    return <ActivityIndicator size={"large"} />;
   }
 
   return (
     <>
       <Provider store={store}>
-        <FlashMessage position={"top"} />
-        <NavigationContainer>
-          <MainAppStack/>
-        </NavigationContainer>
+        <I18nextProvider i18n={i18n}>
+          <FlashMessage position={"top"} />
+          <NavigationContainer>
+            <MainAppStack />
+          </NavigationContainer>
+        </I18nextProvider>
       </Provider>
     </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-
-  },
+  container: {},
 });
