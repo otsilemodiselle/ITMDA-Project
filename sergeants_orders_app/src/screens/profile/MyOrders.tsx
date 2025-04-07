bece8cc0b74395c34866f9760ec19f828f491d4a
Otsile Modiselle <otsilemodiselle@outlook.com>
2025-04-07 19:17:34 +0200

    implement order fetching backend logic into UI

--- sergeants_orders_app/src/screens/profile/MyOrders.tsx
+++ sergeants_orders_app/src/screens/profile/MyOrders.tsx
@@ -1,21 +1,38 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import AppSafeView from '../../components/Views/AppSafeView'
 import HomeHeader from '../../components/headers/homeHeader'
 import OrderCard from '../../components/cards/OrderCard'
 import { FlatList } from 'react-native-gesture-handler'
 import { s, vs } from 'react-native-size-matters'
 import { orders } from '../../data/orders'
+import { fetchUserOrders } from '../../config/dataServices'
+import { getDateFromFireStoreTimeStampObject } from '../../components/headers/dateTimeHelper'
 
 const MyOrders = () => {
+
+    const [ordersList, setOrdersList] = useState([])
+
+    const getOrders = async() => {
+        const response = await fetchUserOrders()
+        setOrdersList(response)
+    }
+
+    useEffect(() => {
+        getOrders()
+    }, [])
+
   return (
     <AppSafeView>
         <HomeHeader/>
         <FlatList style={styles.ordersContainer}
-            data={orders}
+            data={ordersList}
             keyExtractor={(item) => item.id.toString()}
             renderItem={({item}) => {
-                return <OrderCard {...item}/>
+                return (<OrderCard 
+                    cost={item.totalProductsPriceSum}
+                    date={getDateFromFireStoreTimeStampObject(item.createdAt)}
+                />)
             }}
         />
     </AppSafeView>
