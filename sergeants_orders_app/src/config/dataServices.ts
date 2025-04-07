fffade322baae239e7539833e59e10c9c2b32126
Otsile Modiselle <otsilemodiselle@outlook.com>
2025-04-07 19:16:53 +0200

    update backend logic for fetching user orders

--- sergeants_orders_app/src/config/dataServices.ts
+++ sergeants_orders_app/src/config/dataServices.ts
@@ -1,19 +1,37 @@
-import { collection, doc, getDocs } from "firebase/firestore"
-import { db } from "./firebase"
-
-
+import { collection, doc, getDocs } from "firebase/firestore";
+import { auth, db } from "./firebase";
 
 export const getProductsData = async () => {
-    try{
-        const querySnapshot = await getDocs(collection(db, "products"))
-        const list = []
+  try {
+    const querySnapshot = await getDocs(collection(db, "products"));
+    const list = [];
 
-        querySnapshot.forEach((doc) => {
-            list.push(doc.data())
-        })
+    querySnapshot.forEach((doc) => {
+      list.push(doc.data());
+    });
 
-        return list;
-    } catch (error) {
-        console.error("Error fetching data:", error)
-    }
-}
+    return list;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+};
+
+export const fetchUserOrders = async () => {
+  try {
+    // const userIdFromRedux = store.getState().userSlice.userData.uid;
+    const userIDFromFireBase = auth.currentUser?.uid;
+    const userOrdersRef = collection(
+      doc(db, "users", userIDFromFireBase),
+      "orders"
+    );
+    const querySnapshot = await getDocs(userOrdersRef);
+    const orderList = querySnapshot.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    }));
+
+    return orderList;
+  } catch (error) {
+    console.error("Error occurred fetching orders", error);
+  }
+};
