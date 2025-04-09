4007671ee1f07f17fcc9075806171b15b4fd3c8a
Otsile Modiselle <otsilemodiselle@outlook.com>
2025-04-09 19:26:22 +0200

    update i18n script with new English translation key

--- sergeants_orders_app/src/localization/i18n.ts
+++ sergeants_orders_app/src/localization/i18n.ts
@@ -1,9 +1,10 @@
 import i18n from "i18next"
 import { initReactI18next } from "react-i18next"
+import en from "./en.json"
 
 const LANGUAGES = {
     en:{
-
+        translation: en
     },
     af:{
 
