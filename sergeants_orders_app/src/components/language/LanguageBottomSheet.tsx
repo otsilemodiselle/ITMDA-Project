e166fafba6809cb88d4848bfd6fc8c25e8616712
Otsile Modiselle <otsilemodiselle@outlook.com>
2025-04-10 18:24:42 +0200

    refactor static caption to be dynamic on language bottomsheet component

--- sergeants_orders_app/src/components/language/LanguageBottomSheet.tsx
+++ sergeants_orders_app/src/components/language/LanguageBottomSheet.tsx
@@ -1,38 +1,38 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import ActionSheet from 'react-native-actions-sheet'
-import AppText from '../texts/AppText'
-import AppButton from '../buttons/AppButton'
-import { s, vs } from 'react-native-size-matters'
-import { AppColors } from '../../styles/colors'
-import RadioWithTitle from '../inputs/RadioWithTitle'
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import ActionSheet from "react-native-actions-sheet";
+import AppText from "../texts/AppText";
+import AppButton from "../buttons/AppButton";
+import { s, vs } from "react-native-size-matters";
+import { AppColors } from "../../styles/colors";
+import RadioWithTitle from "../inputs/RadioWithTitle";
+import { useTranslation } from "react-i18next";
 
 const LanguageBottomSheet = () => {
+  const { t } = useTranslation();
   return (
     <ActionSheet id="LANG_SHEET">
-        <View style={styles.container}>
-            <AppText style={styles.header}>Change Language</AppText>
-            <RadioWithTitle title="English" selected={true}/>
-            <RadioWithTitle title="Afrikaans" selected={false}/>
-            <RadioWithTitle title="Setswana" selected={false}/>
-            <RadioWithTitle title="isiZulu" selected={false}/>
-            <AppButton
-                title='Confirm'
-            />
-        </View>
+      <View style={styles.container}>
+        <AppText style={styles.header}>{t("languages_header")}</AppText>
+        <RadioWithTitle title="English" selected={true} />
+        <RadioWithTitle title="Afrikaans" selected={false} />
+        <RadioWithTitle title="Setswana" selected={false} />
+        <RadioWithTitle title="isiZulu" selected={false} />
+        <AppButton title={t("language_confirmButton")} />
+      </View>
     </ActionSheet>
-  )
-}
+  );
+};
 
-export default LanguageBottomSheet
+export default LanguageBottomSheet;
 
 const styles = StyleSheet.create({
-    container:{
-        padding: s(16),
-        backgroundColor: AppColors.surface
-    },
-    header:{
-        marginBottom: vs(16),
-        textAlign: "center"
-    }
-})
+  container: {
+    padding: s(16),
+    backgroundColor: AppColors.surface,
+  },
+  header: {
+    marginBottom: vs(16),
+    textAlign: "center",
+  },
+});
