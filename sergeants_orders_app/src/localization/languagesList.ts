3a0f2483790dd8d0d28d6bb37156e7bccdfefe02
Otsile Modiselle <otsilemodiselle@outlook.com>
2025-04-11 11:30:07 +0200

    create a language list script for handling bottom sheet language selection

---
+++ sergeants_orders_app/src/localization/languagesList.ts
@@ -0,0 +1,6 @@
+export const languagesArr = [
+  { code: "en", label: "English" },
+  { code: "af", label: "Afrikaans" },
+  { code: "zu", label: "isiZulu" },
+  { code: "tn", label: "SeTswana" },
+];
