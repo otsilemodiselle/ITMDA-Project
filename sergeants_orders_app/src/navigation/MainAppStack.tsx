56d8e5d672e92288bec4e47954a0c7952ffb2e4d
Otsile Modiselle <otsilemodiselle@outlook.com>
2025-04-11 20:45:41 +0200

    modify app entry point to determine logged in status and authentication

--- sergeants_orders_app/src/navigation/MainAppStack.tsx
+++ sergeants_orders_app/src/navigation/MainAppStack.tsx
@@ -3,21 +3,39 @@ import AuthStack from "./AuthStack";
 import MainAppBottomTabs from "./MainAppBottomTabs";
 import CheckoutScreen from "../components/cart/CheckoutScreen";
 import MyOrders from "../screens/profile/MyOrders";
+import { useDispatch, useSelector } from "react-redux";
+import { setUserData } from "../store/reducers/userSlice";
+import { useEffect } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { RootState } from "../store/store";
 
-
-
-
-const Stack = createStackNavigator()
+const Stack = createStackNavigator();
 
 export default function MainAppStack() {
-    return(
-        <Stack.Navigator
-            screenOptions={{headerShown: false}}
-        >
-            <Stack.Screen name="AuthStack" component={AuthStack}/>
-            <Stack.Screen name="MainAppBottomTabs" component={MainAppBottomTabs}/>
-            <Stack.Screen name="CheckoutScreen" component={CheckoutScreen}/>
-            <Stack.Screen name="MyOrders" component={MyOrders}/>
-        </Stack.Navigator>
-    )
+  const dispatch = useDispatch();
+  const {userData} = useSelector((state: RootState) => state.userSlice)
+  const isUserLoggedIn = async () => {
+    try {
+      const storedUserData = await AsyncStorage.getItem("USER_DATA");
+      console.log(storedUserData)
+      if (storedUserData) {
+        dispatch(setUserData(JSON.parse(storedUserData)));
+      }
+    } catch (error) {
+      console.error("Error reading stored user data", error);
+    }
+  };
+
+  useEffect(() => {
+    isUserLoggedIn();
+  }, []);
+
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName={userData? "MainAppBottomTabs" : "AuthStack"}>
+      <Stack.Screen name="AuthStack" component={AuthStack} />
+      <Stack.Screen name="MainAppBottomTabs" component={MainAppBottomTabs} />
+      <Stack.Screen name="CheckoutScreen" component={CheckoutScreen} />
+      <Stack.Screen name="MyOrders" component={MyOrders} />
+    </Stack.Navigator>
+  );
 }
