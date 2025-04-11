9ab910bf99c9d7ca73d37f389b98444caea0294a
Otsile Modiselle <otsilemodiselle@outlook.com>
2025-04-11 11:31:52 +0200

    complete language setting bottom sheet functionality

--- sergeants_orders_app/src/components/language/LanguageBottomSheet.tsx
+++ sergeants_orders_app/src/components/language/LanguageBottomSheet.tsx
@@ -1,24 +1,36 @@
 import { StyleSheet, Text, View } from "react-native";
-import React from "react";
-import ActionSheet from "react-native-actions-sheet";
+import React, { useState } from "react";
+import ActionSheet, { SheetManager } from "react-native-actions-sheet";
 import AppText from "../texts/AppText";
 import AppButton from "../buttons/AppButton";
 import { s, vs } from "react-native-size-matters";
 import { AppColors } from "../../styles/colors";
 import RadioWithTitle from "../inputs/RadioWithTitle";
 import { useTranslation } from "react-i18next";
+import i18n from "../../localization/i18n";
+import { languagesArr } from "../../localization/languagesList";
+import { lang } from "moment";
 
 const LanguageBottomSheet = () => {
+  const [selectedLang, setSelectedLang] = useState(i18n.language)
+  const onLanguagePress = (code: string) => {
+    setSelectedLang(code)
+  }
+  const handleConfirm = () => {
+    SheetManager.hide("LANG_SHEET")
+    i18n.changeLanguage(selectedLang)
+  }
   const { t } = useTranslation();
   return (
     <ActionSheet id="LANG_SHEET">
       <View style={styles.container}>
         <AppText style={styles.header}>{t("languages_header")}</AppText>
-        <RadioWithTitle title="English" selected={true} />
-        <RadioWithTitle title="Afrikaans" selected={false} />
-        <RadioWithTitle title="Setswana" selected={false} />
-        <RadioWithTitle title="isiZulu" selected={false} />
-        <AppButton title={t("language_confirmButton")} />
+        {
+          languagesArr.map((lang) =>(
+            <RadioWithTitle key={lang.code} title={lang.label} selected={selectedLang === lang.code} onPress={()=>onLanguagePress(lang.code)}/>
+          ))
+        }
+        <AppButton title={t("language_confirmButton")} onPress={handleConfirm} />
       </View>
     </ActionSheet>
   );
