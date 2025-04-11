f692d6954a48d47aa6655d3db4ca027e11c01633
Otsile Modiselle <otsilemodiselle@outlook.com>
2025-04-11 21:49:30 +0200

    update user slice with loading state to handle sign out request

--- sergeants_orders_app/src/store/reducers/userSlice.ts
+++ sergeants_orders_app/src/store/reducers/userSlice.ts
@@ -2,11 +2,13 @@ import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 interface UserState {
-  userData: object;
+  userData: object | null;
+  isLoading: boolean;
 }
 
 const initialState: UserState = {
-  userData: {},
+  userData: null,
+  isLoading: true,
 };
 
 const userSlice = createSlice({
@@ -16,9 +18,13 @@ const userSlice = createSlice({
     setUserData: (state, action: PayloadAction<object>) => {
       state.userData = action.payload;
       AsyncStorage.setItem("USER_DATA", JSON.stringify(action.payload))
+      state.isLoading = false
     },
+    setLoading:(state, action: PayloadAction<boolean>) => {
+        state.isLoading = action.payload
+    }
   },
 });
 
-export const { setUserData } = userSlice.actions;
+export const { setUserData, setLoading } = userSlice.actions;
 export default userSlice.reducer;
