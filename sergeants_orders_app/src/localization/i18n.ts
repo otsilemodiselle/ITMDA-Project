51045d993b08cbdc5c4b9ec1a47663ced961c732
Otsile Modiselle <otsilemodiselle@outlook.com>
2025-04-11 11:31:13 +0200

    add missing language keys to 18 next config file

--- sergeants_orders_app/src/localization/i18n.ts
+++ sergeants_orders_app/src/localization/i18n.ts
@@ -1,19 +1,22 @@
 import i18n from "i18next"
 import { initReactI18next } from "react-i18next"
 import en from "./en.json"
+import af from "./af.json"
+import zu from "./zu.json"
+import tn from "./tn.json"
 
 const LANGUAGES = {
     en:{
         translation: en
     },
     af:{
-
+        translation: af 
     },
     zu:{
-
+        translation: zu
     },
     tn:{
-
+        translation: tn 
     }
 }
 
