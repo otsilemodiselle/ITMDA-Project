bd5ffefca6aa869bcc5ab9760699a0462f72ce43
Otsile Modiselle <otsilemodiselle@outlook.com>
2025-04-11 20:44:40 +0200

    modify userSlice to store logged in user on device

--- sergeants_orders_app/src/store/reducers/userSlice.ts
+++ sergeants_orders_app/src/store/reducers/userSlice.ts
@@ -1,24 +1,24 @@
-
-
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 interface UserState {
-    userData: object;
+  userData: object;
 }
 
 const initialState: UserState = {
-    userData: {}
-}
+  userData: {},
+};
 
-const userSlice = createSlice ({
-    name: "userData",
-    initialState,
-    reducers:{
-        setUserData: (state, action: PayloadAction<object>) =>{
-            state.userData=action.payload
-        }
-    }
-})
+const userSlice = createSlice({
+  name: "userData",
+  initialState,
+  reducers: {
+    setUserData: (state, action: PayloadAction<object>) => {
+      state.userData = action.payload;
+      AsyncStorage.setItem("USER_DATA", JSON.stringify(action.payload))
+    },
+  },
+});
 
-export const {setUserData} = userSlice.actions
-export default userSlice.reducer
+export const { setUserData } = userSlice.actions;
+export default userSlice.reducer;
