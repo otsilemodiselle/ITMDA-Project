f364b33a8e1cbd660305ef83e89506b18a068b77
Otsile Modiselle <otsilemodiselle@outlook.com>
2025-04-12 11:57:07 +0200

    resolve login detection to rely on Firebase Auth status

--- sergeants_orders_app/src/navigation/MainAppStack.tsx
+++ sergeants_orders_app/src/navigation/MainAppStack.tsx
@@ -4,44 +4,47 @@ import MainAppBottomTabs from "./MainAppBottomTabs";
 import CheckoutScreen from "../components/cart/CheckoutScreen";
 import MyOrders from "../screens/profile/MyOrders";
 import { useDispatch, useSelector } from "react-redux";
-import { setLoading, setUserData } from "../store/reducers/userSlice";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { RootState } from "../store/store";
 import { isLoaded, isLoading } from "expo-font";
 import { ActivityIndicator, View } from "react-native";
 import { AppColors } from "../styles/colors";
+import { onAuthStateChanged } from "@firebase/auth";
+import { auth } from "../config/firebase";
 
 const Stack = createStackNavigator();
 
 export default function MainAppStack() {
-  const dispatch = useDispatch();
-  const { userData, isLoading } = useSelector((state: RootState) => state.userSlice);
-  const isUserLoggedIn = async () => {
-    try {
-      const storedUserData = await AsyncStorage.getItem("USER_DATA");
-      console.log(storedUserData);
-      if (storedUserData) {
-        dispatch(setUserData(JSON.parse(storedUserData)));
-      } else {
-        dispatch(setLoading(false));
-      }
-    } catch (error) {
-      console.error("Error reading stored user data", error);
-      dispatch(setLoading(false));
-    }
-  };
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [userData, setUserData] = useState<object | null>(null);
 
   useEffect(() => {
-    isUserLoggedIn();
+    onAuthStateChanged(auth, (userDataFromFireBase) => {
+      if (userDataFromFireBase) {
+        console.log("User is Signed In");
+        setIsLoading(false);
+        setUserData(userDataFromFireBase);
+      } else {
+        console.log("User is Signed Out");
+        setIsLoading(false);
+      }
+    });
   }, []);
 
   if (isLoading) {
     return (
-        <View style={{justifyContent:"center", alignItems:"center", flex:1, backgroundColor:AppColors.mainBackground}}>
-            <ActivityIndicator size={"large"} color={AppColors.accentYellow}/>
-        </View>
-    )
+      <View
+        style={{
+          justifyContent: "center",
+          alignItems: "center",
+          flex: 1,
+          backgroundColor: AppColors.mainBackground,
+        }}
+      >
+        <ActivityIndicator size={"large"} color={AppColors.accentYellow} />
+      </View>
+    );
   }
 
   return (
--- sergeants_orders_app/src/store/reducers/userSlice.ts
+++ sergeants_orders_app/src/store/reducers/userSlice.ts
@@ -3,12 +3,10 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 
 interface UserState {
   userData: object | null;
-  isLoading: boolean;
 }
 
 const initialState: UserState = {
   userData: null,
-  isLoading: true,
 };
 
 const userSlice = createSlice({
@@ -17,14 +15,10 @@ const userSlice = createSlice({
   reducers: {
     setUserData: (state, action: PayloadAction<object>) => {
       state.userData = action.payload;
-      AsyncStorage.setItem("USER_DATA", JSON.stringify(action.payload))
-      state.isLoading = false
+      AsyncStorage.setItem("USER_DATA", JSON.stringify(action.payload));
     },
-    setLoading:(state, action: PayloadAction<boolean>) => {
-        state.isLoading = action.payload
-    }
   },
 });
 
-export const { setUserData, setLoading } = userSlice.actions;
+export const { setUserData } = userSlice.actions;
 export default userSlice.reducer;
